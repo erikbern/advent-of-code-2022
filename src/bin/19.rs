a52d4f291ffe6010a384ae218af020c960890bb8
Erik Bernhardsson <mail@erikbern.com>
2022-12-28 10:07:08 -0500

    Day 19: cleaner

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -3,22 +3,39 @@ use std::collections::HashSet;
 use regex::Regex;
 use std::cmp::min;
 
+type M = [i32; 4];
+
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct S {
-    res: [i32; 4],
-    rob: [i32; 4],
+    res: M,
+    rob: M,
 }
 
-struct Input {
-    ore_ore: i32,
-    cla_ore: i32,
-    obs_ore: i32,
-    obs_cla: i32,
-    geo_ore: i32,
-    geo_obs: i32,
+#[derive(Debug)]
+struct Recipe {
+    material: usize,
+    cost: M,
 }
 
-fn search(i: &Input, n_steps: i32) -> i32 {
+fn add(a: &M, b: &M) -> M {
+    [a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]]
+}
+
+fn sub(a: &M, b: &M) -> M {
+    [a[0] - b[0], a[1] - b[1], a[2] - b[2], a[3] - b[3]]
+}
+
+fn is_ge(a: &M, b: &M) -> bool {
+    a[0] >= b[0] && a[1] >= b[1] && a[2] >= b[2] && a[3] >= b[3]
+}
+
+fn incr_index(a: &M, index: usize) -> M {
+    let mut b = a.clone();
+    b[index] += 1;
+    b
+}
+
+fn search(recipes: &Vec<Recipe>, n_steps: i32) -> i32 {
     let res1 = [0, 0, 0, 0];
     let rob1 = [1, 0, 0, 0];
     let s = S { res: res1, rob: rob1 };
@@ -37,30 +54,14 @@ fn search(i: &Input, n_steps: i32) -> i32 {
             } else if upper_bound < best_lower_bound {
                 continue;
             }
-            let res_n = [s.res[0] + s.rob[0], s.res[1] + s.rob[1], s.res[2] + s.rob[2], s.res[3] + s.rob[3]];
-            if s.res[0] >= i.ore_ore {
-                m_new.insert(S {
-                    res: [res_n[0] - i.ore_ore, res_n[1], res_n[2], res_n[3]],
-                    rob: [s.rob[0] + 1, s.rob[1], s.rob[2], s.rob[3]],
-                });
-            }
-            if s.res[0] >= i.cla_ore {
-                m_new.insert(S {
-                    res: [res_n[0] - i.cla_ore, res_n[1], res_n[2], res_n[3]],
-                    rob: [s.rob[0], s.rob[1] + 1, s.rob[2], s.rob[3]],
-                });
-            }
-            if s.res[0] >= i.obs_ore && s.res[1] >= i.obs_cla {
-                m_new.insert(S {
-                    res: [res_n[0] - i.obs_ore, res_n[1] - i.obs_cla, res_n[2], res_n[3]],
-                    rob: [s.rob[0], s.rob[1], s.rob[2] + 1, s.rob[3]],
-                });
-            }
-            if s.res[0] >= i.geo_ore && s.res[2] >= i.geo_obs {
-                m_new.insert(S {
-                    res: [res_n[0] - i.geo_ore, res_n[1], res_n[2] - i.geo_obs, res_n[3]],
-                    rob: [s.rob[0], s.rob[1], s.rob[2], s.rob[3] + 1],
-                });
+            let res_n = add(&s.res, &s.rob);
+            for r in recipes {
+                if is_ge(&s.res, &r.cost) {
+                    m_new.insert(S {
+                        res: sub(&res_n, &r.cost),
+                        rob: incr_index(&s.rob, r.material),
+                    });
+                }
             }
             m_new.insert(S {res: res_n, rob: s.rob });
         }
@@ -71,27 +72,34 @@ fn search(i: &Input, n_steps: i32) -> i32 {
 
 fn main() {
     let regex = Regex::new(r"\d+").unwrap();
-    let mut inputs = Vec::<(Input, i32)>::new();
+    let mut inputs = Vec::<(i32, Vec<Recipe>)>::new();
 
     for line in get_input() {
         let ints: Vec<i32> = regex.find_iter(&line).map(|item| item.as_str().parse::<i32>().unwrap()).collect();
         let b_id: i32 = ints[0];
-        let input = Input {ore_ore: ints[1], cla_ore: ints[2], obs_ore: ints[3], obs_cla: ints[4], geo_ore: ints[5], geo_obs: ints[6] };
-        inputs.push((input, b_id));
+        let recipes = vec![
+            Recipe {material: 0, cost: [ints[1], 0, 0, 0]},
+            Recipe {material: 1, cost: [ints[2], 0, 0, 0]}, 
+            Recipe {material: 2, cost: [ints[3], ints[4], 0, 0]},
+            Recipe {material: 3, cost: [ints[5], 0, ints[6], 0]},
+        ];
+        inputs.push((b_id, recipes));
     }
 
+    // Part 1
     let mut total_quality_level = 0i32;
-    for (input, b_id) in &inputs {
-        let n_geos = search(&input, 24);
+    for (b_id, recipes) in &inputs {
+        let n_geos = search(&recipes, 24);
         println!("{}: {}", b_id, n_geos);
         total_quality_level += b_id * n_geos;
     }
     println!("-> {}", total_quality_level);
 
+    // Part 2
     let mut product = 1i32;
     let k = min(inputs.len(), 3);
-    for (input, b_id) in &inputs[..k] {
-        let n_geos = search(&input, 32);
+    for (b_id, recipes) in &inputs[..k] {
+        let n_geos = search(&recipes, 32);
         println!("{}: {}", b_id, n_geos);
         product *= n_geos;
     }
